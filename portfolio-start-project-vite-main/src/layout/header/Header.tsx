6daa8fcabeba98450ef9ffab55635833342f1d82
Evhen87 <kushner.aleks@yandex.ru>
2025-05-03 20:49:07 +0300

    Commit 7

--- portfolio-start-project-vite-main/src/layout/header/Header.tsx
+++ portfolio-start-project-vite-main/src/layout/header/Header.tsx
@@ -2,6 +2,8 @@ import styled from "styled-components";
 import {Logo} from "../../components/logo/Logo.tsx";
 import {Menu} from "../../components/menu/Menu.tsx";
 import {Container} from "../../components/Container.ts";
+import {FlexWrapper} from "../../components/FlexWrapper.tsx";
+import {SocialMedia} from "../../components/social_media/SocialMedia.tsx";
 
 const items = ["Home", "About", "Tech Stack", "Projects", "Contact"]
 
@@ -9,16 +11,19 @@ export const Header = () => {
     return (
         <StyledHeader>
             <Container>
-                <Logo />
-                <Menu menuItems={items}/>
+                <FlexWrapper justify={"space-between"} align={"center"} >
+                    <Logo />
+                    <FlexWrapper align={"center"}>
+                        <Menu menuItems={items} />
+                        <SocialMedia />
+                    </FlexWrapper>
+                </FlexWrapper>
             </Container>
         </StyledHeader>
     );
 };
 
 const StyledHeader = styled.header`
-    background-color: #984646;
-    display: flex;
-    justify-content: space-between;
+    background-color: #984646;    
 `
 
